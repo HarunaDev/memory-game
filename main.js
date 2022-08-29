5ffa0cd0692ca7b681ed319839036484a2b36a26
Alvin Haruna <harunaalvin69@gmail.com>
2022-08-29 10:51:35 +0100

    created cards array
    created a card array with key/value pairs of name of image and image location

---
+++ main.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // card-image options
+  const cardArray = [
+    {
+      name: "green hills",
+      img: "img/green-hills.png"
+    },
+    {
+      name: "green hills",
+      img: "img/green-hills.png"
+    },
+    {
+      name: "old house",
+      img: "img/old-house.png"
+    },
+    {
+      name: "old house",
+      img: "img/old-house.png"
+    },
+    {
+      name: "trees & moon",
+      img: "img/trees-moon.png"
+    },
+    {
+      name: "trees & moon",
+      img: "img/trees-moon.png"
+    },
+    {
+      name: "trees & sunset",
+      img: "img/trees-sunset.png"
+    },
+    {
+      name: "trees & sunset",
+      img: "img/trees-sunset.png"
+    }
+  ]
+})
