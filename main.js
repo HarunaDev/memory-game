f50dc10ab56390f5ad6159aef5b1c37f6a22f9dd
Alvin Haruna <harunaalvin69@gmail.com>
2022-09-05 17:40:26 +0100

    created game board
    modified js file and added createBoard function that renders board element into div

--- main.js
+++ main.js
@@ -33,5 +33,21 @@ document.addEventListener("DOMContentLoaded", () => {
       name: "trees & sunset",
       img: "img/trees-sunset.png"
     }
-  ]
+  ];
+
+  const grid = document.querySelector(".grid");
+
+// create game board
+
+function createBoard() {
+  for(let i = 0; i < cardArray.length; i++) {
+    let card = document.createElement("img")
+    card.setAttribute("src", "img/blank.png")
+    card.setAttribute("data-id", 1)
+    grid.appendChild(card)
+  }
+}
+
+createBoard()
 })
+
